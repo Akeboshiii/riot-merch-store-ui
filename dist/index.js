3196bc5dafc4243e887b16f09388a0792ad0400c
Akeboshiii <johnkuraya491@gmail.com>
2025-05-17 22:00:35 +0800

    Add product images and update links in HTML and JavaScript; enhance styles for better UI interaction

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,8 @@
 "use strict";
-var _a;
+var _a, _b;
 (_a = document.querySelector('.hero_cta_button')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
     location.href = "#products_title";
+});
+(_b = document.querySelector('.brand_text')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', () => {
+    location.href = "#";
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 document.querySelector('.hero_cta_button')?.addEventListener('click', () => {
     location.href = "#products_title"
 })
+
+document.querySelector('.brand_text')?.addEventListener('click', () => {
+    location.href = "#"
+})
