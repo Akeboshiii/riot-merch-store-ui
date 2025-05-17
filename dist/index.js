42939c378930c930ec28c9007684f6250a568558
Akeboshiii <johnkuraya491@gmail.com>
2025-05-17 20:32:43 +0800

    improved UI and colors

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,5 @@
 "use strict";
+var _a;
+(_a = document.querySelector('.hero_cta_button')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+    location.href = "#products_title";
+});
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+document.querySelector('.hero_cta_button')?.addEventListener('click', () => {
+    location.href = "#products_title"
+})
